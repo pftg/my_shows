520765630d9ec6ca8808fe4250fb1e4d3bb62e43
Paul Nikitochkin <paul.nikitochkin@gmail.com>
2013-02-04 23:15:07 +0200

    Extracted Episode model. Added colorized gem to prettify output.

--- lib/my_shows/show.rb
+++ lib/my_shows/show.rb
@@ -1,5 +1,8 @@
+require 'hashie'
+require 'hashie/mash'
+
 module MyShows
-  class Show
+  class Show < Hashie::Mash
     def self.client
       @client ||= SidereelClient.new ENV["SIDEREEL_USERNAME"], ENV["SIDEREEL_PASSWORD"]
     end
@@ -8,14 +11,18 @@ module MyShows
       @client = client
     end
 
+    def initialize *attrs
+      super
+      attrs.last[:episode].show = self if attrs.last && attrs.last[:episode]
+    end
+
     def self.next_episodes
       client.tracked_tv_shows.first[1].map(&:tv_show).reject do |show|
+        MyShows.logger.debug "TV Show data: #{show.inspect}"
         show.next_episode.nil? || show.next_episode.is_upcoming
-        #|| (Date.parse(show.next_episode.air_date_utc) >= Time.now.to_date)
       end.map do |show|
-        Hashie::Mash.new name: show.complete_name,
-            season: show.next_episode.season_number,
-            episode: show.next_episode.season_ordinal
+        episode = MyShows::Episode.new(season: show.next_episode.season_number, episode: show.next_episode.season_ordinal)
+        Show.new name: show.complete_name, episode: episode
       end
     end
   end
